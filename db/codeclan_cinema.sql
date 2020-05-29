02aecdc3ea32ec5bb17fd5746492c3095ce1546c
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-29 13:43:56 +0100

    Created tables in sql file

---
+++ db/codeclan_cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE tickets;
+DROP TABLE customers;
+DROP TABLE films;
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    funds INT
+);
+
+CREATE TABLE films (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    price INT
+);
+
+CREATE TABLE tickets (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id) ON DELETE SET NULL,
+    film_id INT REFERENCES films(id) ON DELETE SET NULL
+);
