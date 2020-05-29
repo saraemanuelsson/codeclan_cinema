ff6bb65b39389042d66a4e5bb81f2a1ec9495dcc
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-29 14:06:29 +0100

    Populated the console with a couple of customers, films and tickets

---
+++ console.rb
@@ -0,0 +1,40 @@
+require('pry')
+require_relative('./models/customer')
+require_relative('./models/film')
+require_relative('./models/ticket')
+
+Ticket.delete_all()
+Customer.delete_all()
+Film.delete_all()
+
+harry = Customer.new({ 'name' => 'Harry Potter', 'funds' => 68 })
+hermione = Customer.new({ 'name' => 'Hermione Granger', 'funds' => 37 })
+ron = Customer.new({ 'name' => 'Ron Weasley', 'funds' => 9 })
+
+harry.save()
+hermione.save()
+ron.save()
+
+avengers = Film.new({ 'title' => 'Avengers', 'price' => 8 })
+theory_of_everything = Film.new({ 'title' => 'The Theory of Everything', 'price' => 11 })
+fast_and_furious = Film.new({ 'title' => 'Fast and Furious', 'price' => 4 })
+
+avengers.save()
+theory_of_everything.save()
+fast_and_furious.save()
+
+ticket1 = Ticket.new({ 'customer_id' => harry.id, 'film_id' => avengers.id })
+ticket2 = Ticket.new({ 'customer_id' => hermione.id, 'film_id' => theory_of_everything.id })
+ticket3 = Ticket.new({ 'customer_id' => ron.id, 'film_id' => fast_and_furious.id })
+ticket4 = Ticket.new({ 'customer_id' => harry.id, 'film_id' => fast_and_furious.id })
+ticket5 = Ticket.new({ 'customer_id' => ron.id, 'film_id' => theory_of_everything.id })
+
+ticket1.save()
+ticket2.save()
+ticket3.save()
+ticket4.save()
+ticket5.save()
+
+binding.pry
+
+nil
