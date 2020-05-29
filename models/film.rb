8b558a5e463d8205e16a08d847779e35a5147d91
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-29 14:39:11 +0100

    Add update and delete methods for Film

--- models/film.rb
+++ models/film.rb
@@ -22,4 +22,21 @@ class Film
         @id = id.to_i
     end
 
+    def update()
+        sql = "UPDATE films SET
+        ( title, price )
+        =
+        ( $1, $2 )
+        WHERE id = $3"
+        values = [@title, @price, @id]
+        SqlRunner.run(sql, values)
+    end
+
+    def delete()
+        sql = "DELETE FROM films
+        WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
 end
