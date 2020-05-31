fbeeeb2c3e9263095886a7ab45f6e6b95a94c41c
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-31 09:34:23 +0100

    Add update method to Ticket

--- models/ticket.rb
+++ models/ticket.rb
@@ -22,6 +22,16 @@ class Ticket
         @id = id.to_i
     end
 
+    def update()
+        sql = "UPDATE tickets SET
+        ( customer_id, film_id )
+        =
+        ( $1, $2 )
+        WHERE id = $3"
+        values = [@customer_id, @film_id, @id]
+        SqlRunner.run(sql, values)
+    end
+
     def delete()
         sql = "DELETE FROM tickets
         WHERE id = $1"
