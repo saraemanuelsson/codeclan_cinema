4e8a79ecb0fbae210dcabc77d31dfb697918fdbd
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-31 13:27:18 +0100

    Add save method for Screening

--- models/screening.rb
+++ models/screening.rb
@@ -12,6 +12,17 @@ class Screening
         @id = options['id'].to_i if options['id']
     end
 
+    def save()
+        sql = "INSERT INTO screenings
+        ( time, film_id, tickets )
+        VALUES
+        ( $1, $2, $3 )
+        RETURNING id"
+        values = [@time, @film_id, @tickets]
+        id = SqlRunner.run(sql, values)[0]['id']
+        @id = id.to_i
+    end
+
     def self.delete_all()
         sql = "DELETE FROM screenings"
         SqlRunner.run(sql)
